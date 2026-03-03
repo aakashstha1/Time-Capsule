2666347e2a39fc28601461596264c5b4b52a6627
Aakash Shrestha <aakash.cerestha90@gmail.com>
2026-03-03 10:33:10 +0545

    change

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      " https://time-capsule-4fhl.onrender.com",
+    ],
     credentials: true,
   }),
 );
